924567eac9a94592dfab5a9a17df72a164584ce8
TurikRoma <turikroma15@gmail.com>
2025-08-17 23:14:11 +0300

    Revert "fix"
    This reverts commit 807a92204a625429ba1638099dd545c53dd3aef9.

--- api/meet_listener.py
+++ api/meet_listener.py
@@ -812,7 +812,8 @@ class MeetListenerBot:
             self._initialize_driver()
             
             # 3. Попытка присоединиться к встрече. Передаем флажок дальше.
-            joined_successfully = self.join_meet_as_guest(startup_complete_event)
+            # joined_successfully = self.join_meet_as_guest(startup_complete_event)
+            joined_successfully = True
 
             
             if joined_successfully:

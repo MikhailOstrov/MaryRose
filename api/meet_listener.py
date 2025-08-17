0f19c7835605c707f696e11ce5c4e2f50ab597e2
TurikRoma <turikroma15@gmail.com>
2025-08-17 23:14:20 +0300

    Revert "without join_meet"
    This reverts commit 1ca3d596d710c64ae60e719e1e243f5e28f9063c.

--- api/meet_listener.py
+++ api/meet_listener.py
@@ -808,9 +808,7 @@ class MeetListenerBot:
             self._initialize_driver()
             
             # 3. Попытка присоединиться к встрече. Передаем флажок дальше.
-            # joined_successfully = self.join_meet_as_guest(startup_complete_event)
-            joined_successfully = True
-
+            joined_successfully = self.join_meet_as_guest(startup_complete_event)
             
             if joined_successfully:
                 logger.info(f"[{self.meeting_id}] Успешно вошел в конференцию, запускаю основные процессы.")
@@ -830,8 +828,6 @@ class MeetListenerBot:
                 capture_thread.join()
                 processor_thread.join()
                 monitor_thread.join()
-
-                startup_complete_event()
                 
                 logger.info(f"[{self.meeting_id}] Основные потоки (обработка и захват) завершены.")
             else:

94debf4f27fe582988ca2520b90149119fb544dd
TurikRoma <turikroma15@gmail.com>
2025-08-19 11:53:19 +0300

    Revert "compute type"
    This reverts commit c398bcd054ddee4ab8c0f44ced947e698ee5a06d.

--- config/load_models.py
+++ config/load_models.py
@@ -110,7 +110,7 @@ def load_asr_model():
             token=hf_token
         )
         print(f"ASR модель скачана в: {local_path}")
-    asr_model = WhisperModel(local_path, compute_type="int8_float16")
+    asr_model = WhisperModel(local_path)
     print("ASR model loaded.")
     return asr_model
 
